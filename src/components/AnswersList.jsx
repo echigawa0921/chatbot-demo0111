232f4bff24d076b606a6ae1445d06537510fd448
echigawa0921 <69971834+echigawa0921@users.noreply.github.com>
2021-06-28 22:28:46 +0900

    keyによりエラー解消

--- src/components/AnswersList.jsx
+++ src/components/AnswersList.jsx
@@ -5,7 +5,7 @@ const AnswersList = (props) => {
     return(
         <div className="c-grid__answer">
             {props.answers.map((value, index) => {
-                return  <Answer content={value.content} />
+                return  <Answer content={value.content} key={index.toString()} />
             })}
         </div>
     )
