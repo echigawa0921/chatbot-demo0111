34d101bb7f5bbc5503ad68e440083809f1a59ff7
echigawa0921 <69971834+echigawa0921@users.noreply.github.com>
2021-06-29 18:26:33 +0900

    return条件式でアカウント表示切り替え

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ export default class App extends React.Component {
       dataset: defaultDataset,
       open: false
     }
+    this.selectAnswer = this.selectAnswer.bind(this)
   }
 
 displayNextQuestion = (nextQuestionId) => {
@@ -60,7 +61,7 @@ displayNextQuestion = (nextQuestionId) => {
     <section className="c-section">
       <div className="c-box">
         <Chats chats={this.state.chats} />
-        <AnswersList answers={this.state.answers} />
+        <AnswersList answers={this.state.answers} select={this.selectAnswer} />
       </div>
     </section>
   );
--- src/components/Answer.jsx
+++ src/components/Answer.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
-import { PowerInputSharp } from '@material-ui/icons';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -13,7 +12,7 @@ const Answer = (props) => {
     // const classes = useStyles();
 
     return(
-        <Button variant="contained" color="primary">
+        <Button variant="contained" color="primary" onClick={() => props.select(props.content, props.nextId)}>
             {props.content}
         </Button>
     )
