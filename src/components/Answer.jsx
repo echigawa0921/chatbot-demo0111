bd69abc8386983fb022801b7481e291e8292c8aa
echigawa0921 <69971834+echigawa0921@users.noreply.github.com>
2021-07-01 15:00:24 +0900

    チャットエリアUI修正

--- src/components/Answer.jsx
+++ src/components/Answer.jsx
@@ -1,18 +1,29 @@
 import React from 'react'
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, createStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-        
-    },
-  }));
+const useStyles = makeStyles(() => (
+    createStyles({
+        "button": {
+            borderColor: '#33CC00',
+            color: '#33CC00',
+            fontWeight: 600,
+            marginBottom: '8px',
+            "&:hover": {
+                backgroundColor: '#33CC00',
+                color: '#fff'
+            }
+        }
+    })
+  ));
 
 const Answer = (props) => {
-    // const classes = useStyles();
+    const classes = useStyles();
 
     return(
-        <Button variant="contained" color="primary" onClick={() => props.select(props.content, props.nextId)}>
+        <Button 
+            className={classes.button}
+            variant="outlined" onClick={() => props.select(props.content, props.nextId)}>
             {props.content}
         </Button>
     )
