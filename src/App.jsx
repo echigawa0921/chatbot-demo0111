4c48b2e09b5a27a22bfd7df78b9b0f111fdd4b17
echigawa0921 <69971834+echigawa0921@users.noreply.github.com>
2021-07-02 18:06:17 +0900

    即時関数で表示完了

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
+import defaultDataset from "./dataset";
 import './assets/styles/style.css';
 import { AnswersList, Chats } from './components/index';
 import FormDialog from './components/Forms/FormDialog';
-import {db} from './firebase/index'
 
 export default class App extends React.Component {
   constructor(props){
@@ -11,7 +11,7 @@ export default class App extends React.Component {
       answers:[],
       chats: [],
       currentId: "init",
-      dataset: {},
+      dataset: {defaultDataset},
       open: false
     }
     this.selectAnswer = this.selectAnswer.bind(this)
@@ -83,21 +83,8 @@ displayNextQuestion = (nextQuestionId) => {
   }
 
   componentDidMount() {
-    (async() => {
-      const dataset = this.state.dataset
-
-      await db.collection('question').get().then(snapshots => {
-        snapshots.forEach(doc => {
-          const id = doc.id
-          const data = doc.data()
-          dataset[id] = data
-        })
-      })
-
-      this.initDataset(dataset)
-      const initAnswer = "";
-      this.selectAnswer(initAnswer, this.state.currentId)
-    })()
+    const initAnswer = "";
+    this.selectAnswer(initAnswer, this.state.currentId)
   }
 
   componentDidUpdate(prevProps, prevState, snapshot) {
